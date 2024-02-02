f240c205ab2d4accdbefdfdd08282ac827d09a97
twalepear <145573584+twalepear@users.noreply.github.com>
2024-02-02 22:25:50 +1030

    rock-paper-scissors now loops! yay!

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,21 +1,23 @@
 import random
 
-user_choice = input('Rock (r), Paper (p), Scissors (s)? ')
-choices = ('rock', 'paper', 'scissors')
-roll = random.choice(choices)
-
-print(roll)
-if user_choice == 'r' and roll == 'paper':
-    print("you lose!")
-elif user_choice == 'r' and roll == 'scissors':
-    print("you win!")
-elif user_choice == 'p' and roll == 'rock':
-    print("you win!")
-elif user_choice == 'p' and roll == 'scissors':
-    print("you lose!")
-elif user_choice == 's' and roll == 'rock':
-    print("you lose!")
-elif user_choice == 's' and roll == 'paper':
-    print("you win!")
-else:
-    print("let's play again?")
+response = "y"
+while response.lower() == "y":
+    user_choice = input('rock (r), paper (p), scissors (s)? ')
+    choices = ('rock', 'paper', 'scissors')
+    roll = random.choice(choices)
+    print(roll)
+    if user_choice == 'r' and roll == 'paper':
+        print(f"you lose!")
+    elif user_choice == 'r' and roll == 'scissors':
+        print(f"you win!")
+    elif user_choice == 'p' and roll == 'rock':
+        print(f"you win!")
+    elif user_choice == 'p' and roll == 'scissors':
+        print(f"you lose!")
+    elif user_choice == 's' and roll == 'rock':
+        print(f"you lose!")
+    elif user_choice == 's' and roll == 'paper':
+        print(f"you win!")
+    else:
+        print(f"haha same")
+    response = input("let's play again? (y/n): ")
