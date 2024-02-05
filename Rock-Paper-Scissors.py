be50dc542ef1c08d24492399f8eca82cc0480bf0
twalepear <145573584+twalepear@users.noreply.github.com>
2024-02-05 22:33:54 +1030

    rock-paper-scissors now has more randomised response~

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -5,19 +5,22 @@ while response.lower() == "y":
     user_choice = input('rock (r), paper (p), scissors (s)? ')
     choices = ('rock', 'paper', 'scissors')
     roll = random.choice(choices)
+    they_lost = ('you lose!', 'haha', '😜', '😁')  # for emotes: Windows key + .
+    they_won = ('you win!', 'nice one', '👍', '👏')
+    draw = ('same!', 'haha same~', '🙌', '😂')
     print(roll)
     if user_choice == 'r' and roll == 'paper':
-        print(f"you lose!")
+        print(f"{random.choice(they_lost)}")
     elif user_choice == 'r' and roll == 'scissors':
-        print(f"you win!")
+        print(f"{random.choice(they_won)}")
     elif user_choice == 'p' and roll == 'rock':
-        print(f"you win!")
+        print(f"{random.choice(they_won)}")
     elif user_choice == 'p' and roll == 'scissors':
-        print(f"you lose!")
+        print(f"{random.choice(they_lost)}")
     elif user_choice == 's' and roll == 'rock':
-        print(f"you lose!")
+        print(f"{random.choice(they_lost)}")
     elif user_choice == 's' and roll == 'paper':
-        print(f"you win!")
+        print(f"{random.choice(they_won)}")
     else:
-        print(f"haha same")
+        print(f"{random.choice(draw)}")
     response = input("let's play again? (y/n): ")
